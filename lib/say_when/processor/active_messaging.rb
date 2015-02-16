64a771372e2408f1105aaacf970fb55c1d5d8b4c
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-02-16 11:38:46 -0500

    make activemessaging processor able to load when not required

--- lib/say_when/processor/active_messaging.rb
+++ lib/say_when/processor/active_messaging.rb
@@ -1,22 +1,20 @@
-require 'activemessaging/message_sender'
+if require 'activemessaging/message_sender'
+  module SayWhen
+    module Processor
 
-module SayWhen
-  module Processor
-  
-    class ActiveMessaging < SayWhen::Processor::Base
-      
-      include ::ActiveMessaging::MessageSender
-    
-      def initialize(scheduler)
-        super(scheduler)
-      end
+      class ActiveMessaging < SayWhen::Processor::Base
 
-      # send the job to the other end, then in the a13g processor, call the execute method
-      def process(job)
-        publish(:say_when, {:job_id=>job.id}.to_yaml)
+        include ::ActiveMessaging::MessageSender
+
+        def initialize(scheduler)
+          super(scheduler)
+        end
+
+        # send the job to the other end, then in the a13g processor, call the execute method
+        def process(job)
+          publish(:say_when, {:job_id=>job.id}.to_yaml)
+        end
       end
-      
     end
-
   end
 end
