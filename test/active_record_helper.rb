557d9df46b4a15382dcd9f69769ab1f475570749
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-02-17 15:38:12 -0500

    cleanup use of single quotes and hash keys

--- test/active_record_helper.rb
+++ test/active_record_helper.rb
@@ -6,8 +6,8 @@ require 'sqlite3'
 ActiveRecord::Base.logger = Logger.new('/dev/null')
 
 ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database  => (File.dirname(__FILE__) + "/db/test.db")
+  adapter:  'sqlite3',
+  database: (File.dirname(__FILE__) + '/db/test.db')
 )
 
-require (File.dirname(__FILE__) + "/db/schema.rb")
+require (File.dirname(__FILE__) + '/db/schema.rb')
