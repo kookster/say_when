258fee72aff838ba7d61aa2f59cd39c075570491
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-02-17 12:06:02 -0500

    add railtie for loading migrations and rake tasks

--- lib/say_when.rb
+++ lib/say_when.rb
@@ -10,9 +10,9 @@ require 'say_when/scheduler'
 require 'say_when/processor/active_messaging' if defined?(::ActiveMessaging)
 require 'say_when/processor/shoryuken' if defined?(::Shoryuken)
 require 'say_when/storage/active_record/job' if defined?(::ActiveRecord)
+require 'say_when/railtie' if defined?(Rails)
 
 module SayWhen
-
   def SayWhen.logger=(logger)
     @@logger = logger
   end
@@ -27,5 +27,4 @@ module SayWhen
     end
     @@logger
   end
-
 end
