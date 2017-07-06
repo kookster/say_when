eebf5bb6346c7b7e9c2f921664a8a79c73373ed1
Andrew Kuklewicz <andrew@beginsinwonder.com>
2017-07-06 13:42:48 -0400

    use new executed methods

--- test/say_when/processor/active_job_strategy_test.rb
+++ test/say_when/processor/active_job_strategy_test.rb
@@ -9,7 +9,7 @@ describe SayWhen::Processor::ActiveJobStrategy do
   let(:processor) { SayWhen::Processor::ActiveJobStrategy }
 
   before {
-    SayWhen::Test::TestTask.executed = false
+    SayWhen::Test::TestTask.reset
   }
 
   it 'process a memory stored job' do
@@ -21,11 +21,11 @@ describe SayWhen::Processor::ActiveJobStrategy do
       job_method: 'execute'
     }
 
-    SayWhen::Test::TestTask.wont_be :executed
+    SayWhen::Test::TestTask.wont_be :executed?
 
     job = SayWhen::Storage::MemoryStrategy.create(options)
     processor.process(job)
 
-    SayWhen::Test::TestTask.must_be :executed
+    SayWhen::Test::TestTask.must_be :executed?
   end
 end
