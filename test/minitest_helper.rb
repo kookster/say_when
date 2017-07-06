07c872ef4422d6bcb2a549def30b7b6b8e101f4c
Andrew Kuklewicz <andrew@beginsinwonder.com>
2017-07-06 13:37:55 -0400

    move more config to test helper

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 
 ENV['RAILS_ENV'] ||= 'test'
+ENV['CELLULOID_BACKPORTED'] ||= 'false'
 
 require 'simplecov'
 SimpleCov.start #'rails'
@@ -20,21 +21,13 @@ require 'minitest/mock'
 require 'fileutils'
 
 require 'active_support'
+require 'active_job'
 
-module SayWhen
-  module Test
-    class TestTask
+require 'celluloid/test'
 
-      cattr_accessor :executed
-      @@executed = false
+ActiveJob::Base.queue_adapter = :inline
 
-      def execute(data)
-        @@executed = true
-        data[:result] || 0
-      end
-    end
-  end
-end
+Celluloid.boot
 
 SayWhen.configure do |options|
   options[:storage_strategy]   = :memory
@@ -42,3 +35,40 @@ SayWhen.configure do |options|
 end
 
 SayWhen.logger = Logger.new('/dev/null')
+
+module SayWhen
+  module Test
+    class TestTask
+      @@executed = false
+
+      def self.reset
+        @@executed = false
+      end
+
+      def self.execute(data)
+        @@executed = true
+        data[:result] || 0
+      end
+
+      def self.executed?
+        @@executed
+      end
+    end
+
+    class TestActsAsScheduled
+      @@_has_many = false
+
+      def self.has_many_called?
+        @@_has_many
+      end
+
+      def self.has_many(*args)
+        @@_has_many = true
+      end
+
+      require 'say_when/storage/active_record_strategy'
+      include SayWhen::Storage::ActiveRecordStrategy::Acts
+      acts_as_scheduled
+    end
+  end
+end
